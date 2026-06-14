645924336005e144da6322e8202c56c6770dec33
Mitul1779 <mitulgarg1779@gmail.com>
2026-06-14 12:55:45 +0530

    basic structure and functions

---
+++ weather.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+def load_history():
+    pass
+
+def save_history():             #only keep last 5
+    pass
+
+def show_history():
+    pass
+
+def show_last_saved():
+    pass
+
+def get_weather():
+    pass
+
+def get_aqi():
+    pass
+
+def get_aqi_health():
+    pass
+
+def display_weather():
+    pass
