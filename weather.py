c4d0a39b76c0060d3fbbc84f966f948ea37a7ec2
Mitul1779 <mitulgarg1779@gmail.com>
2026-06-14 17:14:08 +0530

    api left

--- weather.py
+++ weather.py
@@ -2,25 +2,48 @@ import os
 import json
 
 def load_history():
+    if os.path.exists("history.json"):
+        try:
+            with open("history.json", "r") as file:
+                history = json.load(file)
+        except json.JSONDecodeError:
+            history = []
+    else:
+        history = []
+    return history
+
+def save_history(history):             #only keep last 5
+    with open("history.json", "w") as file:
+        json.dump(history[-5:], file)
+
+def show_history(history):
+    for record in history:
+        display_weather(record)
+
+def show_last_saved(history):
+    if history:
+        display_weather(history[-1])
+
+def get_weather(city):
     pass
 
-def save_history():             #only keep last 5
+def get_aqi(lat, lon):
     pass
 
-def show_history():
-    pass
+def get_aqi_health(aqi):
+    match aqi:
+        case 1:
+            return "Good"
+        case 2:
+            return "Fair"
+        case 3:
+            return "Moderate"
+        case 4:
+            return "Poor"
+        case 5:
+            return "Very Poor"
+        case _:
+            return "Unknown"
 
-def show_last_saved():
-    pass
-
-def get_weather():
-    pass
-
-def get_aqi():
-    pass
-
-def get_aqi_health():
-    pass
-
-def display_weather():
+def display_weather(record):
     pass
